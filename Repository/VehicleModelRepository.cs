2f1ddd3489d73ccede36abd00374a3cbffa6b3d3
Nina <ninapetrusic176@gmail.com>
2023-10-12 00:14:33 +0200

    Todo: implement repositories

---
+++ Repository/VehicleModelRepository.cs
@@ -0,0 +1,13 @@
+﻿using Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class VehicleModelRepository : IVehicleModelRepository
+    {
+    }
+}
