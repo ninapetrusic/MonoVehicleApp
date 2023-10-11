50b22c8d009dcff6e44fec71443511553ff6483a
Nina <ninapetrusic176@gmail.com>
2023-10-12 00:11:59 +0200

    Add models

---
+++ Model/VehicleMake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Common;
+
+namespace Model
+{
+    public class VehicleMake : IVehicleMake
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Abrv {  get; set; }
+
+        public VehicleMake(string name) {
+            Name = name;
+        }
+        public VehicleMake(string name, string abrv)
+        {
+            Name = name;
+            Abrv = abrv;
+        }
+    }
+}
---
+++ Model/VehicleModel.cs
@@ -0,0 +1,29 @@
+﻿using Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class VehicleModel : IVehicleModel
+    {
+        public int Id { get; set; }
+        public int VehicleMakeId { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Abrv { get; set; }
+
+        public VehicleModel(string name, int vehicleMakeId) 
+        {
+            Name = name;
+            VehicleMakeId = vehicleMakeId;
+        }
+        public VehicleModel(string name, string abrv, int vehicleMakeId)
+        {
+            Name = name;
+            Abrv = abrv;
+            VehicleMakeId = vehicleMakeId;
+        }
+    }
+}
