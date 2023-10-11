8732d88497d96e4adce3639b084571f1f8b6221c
Nina <ninapetrusic176@gmail.com>
2023-10-12 00:15:59 +0200

    Add repository interfaces

---
+++ Repository.Common/IVehicleMakeRepository.cs
@@ -0,0 +1,19 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Common
+{
+    public interface IVehicleMakeRepository : IDisposable
+    {
+        IEnumerable<VehicleMake> GetVehicleMakes();
+        VehicleMake GetVehicleMakeById(int id);
+        void InsertVehicleMake(VehicleMake vehicleMake);
+        void DeleteVehicleMake(int id);
+        void UpdateVehicleMake(VehicleMake vehicleMake);
+        void Save();
+    }
+}
---
+++ Repository.Common/IVehicleModelRepository.cs
@@ -0,0 +1,19 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Common
+{
+    public interface IVehicleModelRepository : IDisposable
+    {
+        IEnumerable<VehicleModel> GetVehicleModels();
+        VehicleModel GetVehicleModelById(int id);
+        void InsertVehicleModel(VehicleModel vehicleModel);
+        void DeleteVehicleModel(int id);
+        void UpdateVehicleModel(VehicleModel vehicleModel);
+        void Save();
+    }
+}
