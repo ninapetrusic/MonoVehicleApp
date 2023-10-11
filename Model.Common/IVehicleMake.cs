84d1d4577f97b2a2a28196fbf0a5785aaef57662
Nina <ninapetrusic176@gmail.com>
2023-10-12 00:12:17 +0200

    Add model interfaces

---
+++ Model.Common/IVehicleMake.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Common
+{
+    public interface IVehicleMake
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Abrv { get; set; }
+    }
+}
---
+++ Model.Common/IVehicleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Common
+{
+    public interface IVehicleModel
+    {
+        public int Id { get; set; }
+        public int VehicleMakeId { get; set; }
+        public string Name { get; set; }
+        public string? Abrv { get; set; }
+    }
+}
