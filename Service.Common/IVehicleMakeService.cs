c5ffefbc4aec1149114c2e6d8bf3da2f550e3db0
Nina <ninapetrusic176@gmail.com>
2023-10-26 15:47:33 +0200

    Add interfaces for business logic

---
+++ Service.Common/IVehicleMakeService.cs
@@ -0,0 +1,18 @@
+﻿using Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Common
+{
+    public interface IVehicleMakeService
+    {
+        Task<List<IVehicleMake>> GetVehicleMakesAsync();
+        Task<IVehicleMake> GetVehicleMakeByIdAsync(int id);
+        Task<string> InsertVehicleMakeAsync(IVehicleMake vehicleMake);
+        Task<string> UpdateVehicleMakeAsync(int id, IVehicleMake vehicleMake);
+        Task<bool> DeleteVehicleMakeAsync(int id);
+    }
+}
---
+++ Service.Common/IVehicleModelService.cs
@@ -0,0 +1,18 @@
+﻿using Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Common
+{
+    public interface IVehicleModelService
+    {
+        Task<List<IVehicleModel>> GetVehicleModelsAsync();
+        Task<IVehicleModel> GetVehicleModelByIdAsync(int id);
+        Task<string> InsertVehicleModelAsync(IVehicleModel vehicleModel);
+        Task<string> UpdateVehicleModelAsync(int id, IVehicleModel vehicleModel);
+        Task<bool> DeleteVehicleModelAsync(int id);
+    }
+}
