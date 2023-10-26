0ffc81cd77b09d49e066ae52e2edaf75dff82520
Nina <ninapetrusic176@gmail.com>
2023-10-26 15:46:09 +0200

    Add business logic

---
+++ Service/VehicleMakeService.cs
@@ -0,0 +1,46 @@
+﻿using Model.Common;
+using Repository.Common;
+using Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class VehicleMakeService : IVehicleMakeService
+    {
+        IVehicleMakeRepository vehicleMakeRepository;
+
+        public VehicleMakeService(IVehicleMakeRepository vehicleMakeRepository)
+        {
+            this.vehicleMakeRepository = vehicleMakeRepository;
+        }
+
+        public async Task<bool> DeleteVehicleMakeAsync(int id)
+        {
+            return await vehicleMakeRepository.DeleteVehicleMakeAsync(id);
+        }
+
+        public Task<IVehicleMake> GetVehicleMakeByIdAsync(int id)
+        {
+            return await vehicleMakeRepository.GetVehicleMakeByIdAsync(id);
+        }
+
+        public async Task<List<IVehicleMake>> GetVehicleMakesAsync()
+        {
+            return await vehicleMakeRepository.GetVehicleMakesAsync();
+        }
+
+        public async Task<string> InsertVehicleMakeAsync(IVehicleMake vehicleMake)
+        {
+            return await vehicleMakeRepository.InsertVehicleMakeAsync(vehicleMake);
+        }
+
+        public async Task<string> UpdateVehicleMakeAsync(int id, IVehicleMake vehicleMake)
+        {
+            return await vehicleMakeRepository.UpdateVehicleMakeAsync(id, vehicleMake);
+        }
+    }
+}
---
+++ Service/VehicleModelService.cs
@@ -0,0 +1,44 @@
+﻿using Model.Common;
+using Repository.Common;
+using Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class VehicleModelService : IVehicleModelService
+    {
+        IVehicleModelRepository vehicleModelRepository;
+        public VehicleModelService(IVehicleModelRepository vehicleModelRepository)
+        {
+            this.vehicleModelRepository = vehicleModelRepository;
+        }
+        public async Task<bool> DeleteVehicleModelAsync(int id)
+        {
+            return await vehicleModelRepository.DeleteVehicleModelAsync(id);
+        }
+
+        public async Task<IVehicleModel> GetVehicleModelByIdAsync(int id)
+        {
+            return await vehicleModelRepository.GetVehicleModelByIdAsync(id);
+        }
+
+        public async Task<List<IVehicleModel>> GetVehicleModelsAsync()
+        {
+            return await vehicleModelRepository.GetVehicleModelsAsync();
+        }
+
+        public async Task<string> InsertVehicleModelAsync(IVehicleModel vehicleModel)
+        {
+            return await vehicleModelRepository.InsertVehicleModelAsync(vehicleModel);
+        }
+
+        public async Task<string> UpdateVehicleModelAsync(int id, IVehicleModel vehicleModel)
+        {
+            return await vehicleModelRepository.UpdateVehicleModelAsync(id, vehicleModel);
+        }
+    }
+}
