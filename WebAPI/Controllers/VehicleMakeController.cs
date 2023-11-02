f72bfa618173b9c7060b5ee8039a2cfd01393294
Nina <ninapetrusic176@gmail.com>
2023-11-02 15:48:59 +0100

    Add API methods

--- WebAPI/Controllers/VehicleMakeController.cs
+++ WebAPI/Controllers/VehicleMakeController.cs
@@ -9,7 +9,6 @@ namespace WebAPI.Controllers
     public class VehicleMakeController : ControllerBase
     {
         private IVehicleMakeService _makeService;
-
         public VehicleMakeController(IVehicleMakeService makeService)
         {
             _makeService = makeService;
@@ -19,6 +18,34 @@ namespace WebAPI.Controllers
         {
             IEnumerable<IVehicleMake> makes = await _makeService.GetVehicleMakesAsync();
             return Ok(makes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMakeById(int id)
+        {
+            IVehicleMake make = await _makeService.GetVehicleMakeByIdAsync(id);
+            return Ok(make);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMake(IVehicleMake vehicleMake)
+        {
+            bool created = await _makeService.InsertVehicleMakeAsync(vehicleMake);
+            return (created) ? Ok(vehicleMake) : BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMake(int id, IVehicleMake vehicleMake)
+        {
+            await _makeService.UpdateVehicleMakeAsync(id, vehicleMake);
+            return Ok(vehicleMake);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMake(int id)
+        {
+            await _makeService.DeleteVehicleMakeAsync(id);
+            return Ok();
         }
     }
 }
--- WebAPI/Controllers/VehicleModelController.cs
+++ WebAPI/Controllers/VehicleModelController.cs
@@ -9,7 +9,6 @@ namespace WebAPI.Controllers
     public class VehicleModelController : ControllerBase
     {
         private IVehicleModelService _modelService;
-
         public VehicleModelController(IVehicleModelService modelService)
         {
             _modelService = modelService;
@@ -20,6 +19,34 @@ namespace WebAPI.Controllers
         {
             IEnumerable<IVehicleModel> models = await _modelService.GetVehicleModelsAsync();
             return Ok(models);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetModelById(int id)
+        {
+            IVehicleModel model = await _modelService.GetVehicleModelByIdAsync(id);
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateModel(IVehicleModel vehicleModel)
+        {
+            bool created = await _modelService.InsertVehicleModelAsync(vehicleModel);
+            return (created) ? Ok(vehicleModel) : BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateModel(int id, IVehicleModel vehicleModel)
+        {
+            await _modelService.UpdateVehicleModelAsync(id, vehicleModel);
+            return Ok(vehicleModel);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteModel(int id)
+        {
+            await _modelService.DeleteVehicleModelAsync(id);
+            return Ok();    
         }
     }
 }
