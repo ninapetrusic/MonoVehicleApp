45962898564a196b9dbd93b115e4ce9ce407e05b
Nina <ninapetrusic176@gmail.com>
2023-11-06 16:52:39 +0100

    Automapper

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,7 +1,10 @@
 using Autofac;
+using Autofac.Core;
 using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
 using AutoMapper.Contrib.Autofac.DependencyInjection;
 using DAL;
+using Model;
 using Model.Common;
 using Repository;
 using Repository.Common;
@@ -13,8 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(
     builder =>
-    {
-        builder.RegisterAutoMapper(typeof(Program).Assembly);
+    {     
         builder.RegisterType<Model.VehicleModel>().As<IVehicleModel>();
         builder.RegisterType<Model.VehicleMake>().As<IVehicleMake>();
         builder.RegisterType<VehicleMakeRepository>().As<IVehicleMakeRepository>();
@@ -22,9 +24,14 @@ builder.Host.ConfigureContainer<ContainerBuilder>(
         builder.RegisterType<VehicleMakeService>().As<IVehicleMakeService>();
         builder.RegisterType<VehicleModelService>().As<IVehicleModelService>();
         builder.RegisterType<VehicleContext>();
-
     });
-// Add services to the container.
+// Add services to the container. 
+
+var config = new MapperConfiguration(c => {
+    c.AddProfile<VehicleMakeProfile>();
+    c.AddProfile<VehicleModelProfile>();
+});
+builder.Services.AddSingleton<IMapper>(s => config.CreateMapper());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
