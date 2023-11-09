68e00f2274118df0ccf034a8871e3f74fd1fa29d
Nina <ninapetrusic176@gmail.com>
2023-11-09 13:08:12 +0100

    Add paging, sorting, filtering

---
+++ Common/QueryParams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class QueryParams
+    {
+        private const int _maxPageSize = 20;
+        public int Page { get; set; } = 1;
+        private int _pageSize = _maxPageSize;
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = (value > _maxPageSize) ? _maxPageSize : value; }
+        }
+        public string? OrderBy { get; set; } = "Id";
+        public string FilterName { get; set; } = "";
+    }
+}
---
+++ Common/QueryParamsFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class QueryParamsFunctions
+    {
+        public static bool HasPrev(this QueryParams queryParams)
+        {
+            return (queryParams.Page > 1);
+        }
+        public static bool HasNext(this QueryParams queryParams, int total)
+        {
+            return (queryParams.Page < GetTotalPages(queryParams, total));
+        }
+        public static int GetTotalPages(this QueryParams queryParams, int total)
+        {
+            return (int)Math.Ceiling(total / (double)queryParams.PageSize);
+        }
+        public static bool IsDesc(this QueryParams queryParams)
+        {
+            if(!String.IsNullOrEmpty(queryParams.OrderBy))
+            {
+                return queryParams.OrderBy.Split(' ').Last().ToLowerInvariant().StartsWith("desc");
+            }
+            return false;
+        }
+    }
+}
