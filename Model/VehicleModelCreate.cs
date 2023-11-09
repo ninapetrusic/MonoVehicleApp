559d8c0460ff7bbff6be78adece1ced0c2753f2e
Nina <ninapetrusic176@gmail.com>
2023-11-09 13:09:05 +0100

    Add models for post and put requests

---
+++ Model/VehicleModelCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class VehicleModelCreate
+    {
+        [Required]
+        public String Name { get; set; }
+        public String Abrv {  get; set; }
+        [Required]
+        public int VehicleMakeId { get; set; }
+    }
+}
