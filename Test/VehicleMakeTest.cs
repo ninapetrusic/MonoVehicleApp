ae624064abe32754fb5fdb13d35ab7a885e8758d
Nina <ninapetrusic176@gmail.com>
2023-11-11 17:38:45 +0100

    Unit test

---
+++ Test/VehicleMakeTest.cs
@@ -0,0 +1,161 @@
+﻿using AutoMapper;
+using Model;
+using Model.Common;
+using Moq;
+using Repository.Common;
+using Service;
+using Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test
+{
+    public class VehicleMakeTest
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+        private static MapperConfiguration mapperConfig = new MapperConfiguration(
+        cfg =>
+        {
+            cfg.AddProfile<VehicleMakeProfile>();
+            cfg.AddProfile<VehicleModelProfile>();
+        });
+        private readonly IMapper _mapper = mapperConfig.CreateMapper();
+
+        public VehicleMakeTest()
+        {
+            _unitOfWork = new Mock<IUnitOfWork>();
+        }
+
+        [Fact]
+        public async Task ServiceGetAllTest()
+        {
+            var query = new Common.QueryParams();
+            var data = GetData(); //DAL
+            var dataModel = _mapper.Map<IEnumerable<DAL.VehicleMake>, IEnumerable<IVehicleMake>>(data);
+            _unitOfWork.Setup(x => x.vehicleMakeRepo.GetAllAsync(query)).ReturnsAsync(data);
+            var makeService = new VehicleMakeService(_unitOfWork.Object, _mapper);
+            var result = await makeService.GetVehicleMakesAsync(query).ConfigureAwait(true);
+
+            Assert.NotNull(result);
+            Assert.Equal(GetData().Count(), result.Count());
+            Assert.True(dataModel.GetType() == result.GetType());
+        }
+
+        [Fact]
+        public async Task ServiceGetByIdAsync()
+        {
+            var data = GetData();
+            var dataModel = _mapper.Map<IEnumerable<DAL.VehicleMake>, IEnumerable<IVehicleMake>>(data);
+            _unitOfWork.Setup(x => x.vehicleMakeRepo.GetByIdAsync(2)).ReturnsAsync(data.ElementAt(1));
+            var makeService = new VehicleMakeService(_unitOfWork.Object, _mapper);
+            var result = await makeService.GetVehicleMakeByIdAsync(2).ConfigureAwait(true);
+
+            Assert.NotNull(result);
+            Assert.True(dataModel.ElementAt(1).Id == result.Id);
+            Assert.True(dataModel.ElementAt(1).GetType() == result.GetType());
+        }
+
+        [Fact]
+        public async Task ServiceGetByIdAsync_NotExisting()
+        {
+            _unitOfWork.Setup(x => x.vehicleMakeRepo.GetByIdAsync(1)).ReturnsAsync(new DAL.VehicleMake());
+            var makeService = new VehicleMakeService(_unitOfWork.Object, _mapper);
+            var result = await makeService.GetVehicleMakeByIdAsync(1).ConfigureAwait(true);
+
+            Assert.IsAssignableFrom<IVehicleMake>(result);
+            Assert.True(result.Id == 0);
+            Assert.True(result.Name == null);
+
+        }
+
+        [Fact]
+        public async Task ServiceCreateMake_Ok()
+        {
+            VehicleMakeCreate data = new VehicleMakeCreate { Name = "name1", Abrv = "abrv1" };
+            _unitOfWork.Setup(x => x.CommitAsync()).ReturnsAsync(1);
+            _unitOfWork.Setup(x => x.AddAsync(It.IsAny<DAL.VehicleMake>())).ReturnsAsync(1);
+            IVehicleMakeService service = new VehicleMakeService(_unitOfWork.Object, _mapper);
+            var result = await service.InsertVehicleMakeAsync(data).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task ServiceCreateMake_BadReq()
+        {
+            VehicleMakeCreate data = new VehicleMakeCreate { Name = "name1", Abrv = "abrv1" };
+            _unitOfWork.Setup(x => x.AddAsync(It.IsAny<DAL.VehicleMake>())).ReturnsAsync(0);
+            IVehicleMakeService service = new VehicleMakeService(_unitOfWork.Object, _mapper);
+            var result = await service.InsertVehicleMakeAsync(data).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task ServiceDeleteMake_Ok()
+        {
+            DAL.VehicleMake data = GetData().ElementAt(1);
+            _unitOfWork.Setup(x => x.vehicleMakeRepo.GetByIdAsync(2)).ReturnsAsync(data);
+            _unitOfWork.Setup(x => x.DeleteAsync(data)).ReturnsAsync(1);
+            _unitOfWork.Setup(x => x.CommitAsync()).ReturnsAsync(1);
+            IVehicleMakeService service = new VehicleMakeService(_unitOfWork.Object, _mapper);
+            var result = await service.DeleteVehicleMakeAsync(2).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.True(result);
+
+        }
+        [Fact]
+        public async Task ServiceDeleteMake_BadReq()
+        {
+            _unitOfWork.Setup(x => x.vehicleMakeRepo.GetByIdAsync(2)).ReturnsAsync(new DAL.VehicleMake());
+            IVehicleMakeService service = new VehicleMakeService(_unitOfWork.Object, _mapper);
+            var result = await service.DeleteVehicleMakeAsync(2).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.False(result);
+        }
+        [Fact]
+        public async Task ServiceUpdateMake_Ok()
+        {
+            DAL.VehicleMake data = GetData().ElementAt(1);
+            _unitOfWork.Setup(x => x.vehicleMakeRepo.GetByIdAsync(2)).ReturnsAsync(data);
+            _unitOfWork.Setup(x => x.UpdateAsync(It.IsAny<DAL.VehicleMake>())).ReturnsAsync(1);
+            _unitOfWork.Setup(x => x.CommitAsync()).ReturnsAsync(1);
+            IVehicleMakeService service = new VehicleMakeService(_unitOfWork.Object, _mapper);
+            var result = await service.UpdateVehicleMakeAsync(2, new VehicleMakeCreate { Name = "name" }).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.True(result);
+        }
+        [Fact]
+        public async Task ServiceUpdateMake_BadReq()
+        {
+            DAL.VehicleMake data = GetData().ElementAt(1);
+            _unitOfWork.Setup(x => x.vehicleMakeRepo.GetByIdAsync(2)).ReturnsAsync(data);
+            _unitOfWork.Setup(x => x.UpdateAsync(It.IsAny<DAL.VehicleMake>())).ReturnsAsync(0);
+            IVehicleMakeService service = new VehicleMakeService(_unitOfWork.Object, _mapper);
+            var result = await service.UpdateVehicleMakeAsync(2, new VehicleMakeCreate { Name = "name" }).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.False(result);
+        }
+
+        private static IEnumerable<DAL.VehicleMake> GetData()
+        {
+            IEnumerable<DAL.VehicleMake> data = new List<DAL.VehicleMake>
+            {
+                new DAL.VehicleMake { Id = 1, Name = "name1", Abrv = "abrv1" },
+                new DAL.VehicleMake { Id = 2, Name = "name2", Abrv = null },
+                new DAL.VehicleMake { Id = 3, Name = "name3", Abrv = "abrv3" },
+            };
+            return data;
+        }
+    }
+}
---
+++ Test/VehicleModelTest.cs
@@ -0,0 +1,163 @@
+﻿using AutoMapper;
+using DAL;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Model;
+using Model.Common;
+using Moq;
+using Repository;
+using Repository.Common;
+using Service;
+using Service.Common;
+using System.Diagnostics;
+using WebAPI.Controllers;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Test
+{
+    public class VehicleModelTest
+    {  
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+        private static MapperConfiguration mapperConfig = new MapperConfiguration(
+        cfg =>
+        {
+            cfg.AddProfile<VehicleMakeProfile>();
+            cfg.AddProfile<VehicleModelProfile>();
+        });
+        private readonly IMapper _mapper = mapperConfig.CreateMapper();
+
+        public VehicleModelTest() {
+            _unitOfWork = new Mock<IUnitOfWork>();
+        }
+
+        [Fact]
+        public async Task ServiceGetAllTest()
+        {
+            var query = new Common.QueryParams();
+            var data = GetData(); //DAL
+            var dataModel = _mapper.Map<IEnumerable<DAL.VehicleModel>, IEnumerable<IVehicleModel>>(data);
+            _unitOfWork.Setup(x => x.vehicleModelRepo.GetAllAsync(query)).ReturnsAsync(data);
+            var modelService = new VehicleModelService(_unitOfWork.Object, _mapper);
+            var result = await modelService.GetVehicleModelsAsync(query).ConfigureAwait(true);
+
+            Assert.NotNull(result);
+            Assert.Equal(GetData().Count(), result.Count());
+            Assert.True(dataModel.GetType() == result.GetType());
+        }
+
+        [Fact]
+        public async Task ServiceGetByIdAsync()
+        {
+            var data = GetData();
+            var dataModel = _mapper.Map<IEnumerable<DAL.VehicleModel>, IEnumerable<IVehicleModel>>(data);
+            _unitOfWork.Setup(x => x.vehicleModelRepo.GetByIdAsync(2)).ReturnsAsync(data.ElementAt(1));
+            var modelService = new VehicleModelService(_unitOfWork.Object, _mapper);
+            var result = await modelService.GetVehicleModelByIdAsync(2).ConfigureAwait(true);
+
+            Assert.NotNull(result);
+            Assert.True(dataModel.ElementAt(1).Id == result.Id);
+            Assert.True(dataModel.ElementAt(1).GetType() == result.GetType());
+        }
+
+        [Fact]
+        public async Task ServiceGetByIdAsync_NotExisting()
+        {
+            _unitOfWork.Setup(x => x.vehicleModelRepo.GetByIdAsync(1)).ReturnsAsync(new DAL.VehicleModel());
+            var modelService = new VehicleModelService(_unitOfWork.Object, _mapper);
+            var result = await modelService.GetVehicleModelByIdAsync(1).ConfigureAwait(true);
+
+            Assert.IsAssignableFrom<IVehicleModel>(result);
+            Assert.True(result.Id == 0);
+            Assert.True(result.Name == null);
+
+        }
+
+        [Fact]
+        public async Task ServiceCreateModel_Ok()
+        {
+            VehicleModelCreate data = new VehicleModelCreate { Name = "name1", Abrv = "abrv1", VehicleMakeId = 1 };
+            _unitOfWork.Setup(x => x.CommitAsync()).ReturnsAsync(1);
+            _unitOfWork.Setup(x => x.AddAsync(It.IsAny<DAL.VehicleModel>())).ReturnsAsync(1);
+            IVehicleModelService service = new VehicleModelService(_unitOfWork.Object,_mapper);
+            var result = await service.InsertVehicleModelAsync(data).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task ServiceCreateModel_BadReq()
+        {
+            VehicleModelCreate data = new VehicleModelCreate { Name = "name1", Abrv = "abrv1", VehicleMakeId = 1 };
+            _unitOfWork.Setup(x => x.AddAsync(It.IsAny<DAL.VehicleModel>())).ReturnsAsync(0);
+            IVehicleModelService service = new VehicleModelService(_unitOfWork.Object, _mapper);
+            var result = await service.InsertVehicleModelAsync(data).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task ServiceDeleteModel_Ok()
+        {
+            DAL.VehicleModel data = GetData().ElementAt(1);
+            _unitOfWork.Setup(x => x.vehicleModelRepo.GetByIdAsync(2)).ReturnsAsync(data);
+            _unitOfWork.Setup(x => x.DeleteAsync(data)).ReturnsAsync(1);
+            _unitOfWork.Setup(x => x.CommitAsync()).ReturnsAsync(1);
+            IVehicleModelService service = new VehicleModelService(_unitOfWork.Object, _mapper);
+            var result = await service.DeleteVehicleModelAsync(2).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.True(result);
+
+        }
+        [Fact]
+        public async Task ServiceDeleteModel_BadReq()
+        {
+            _unitOfWork.Setup(x => x.vehicleModelRepo.GetByIdAsync(2)).ReturnsAsync(new DAL.VehicleModel());
+            IVehicleModelService service = new VehicleModelService(_unitOfWork.Object, _mapper);
+            var result = await service.DeleteVehicleModelAsync(2).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.False(result);
+        }
+        [Fact]
+        public async Task ServiceUpdateModel_Ok()
+        {
+            DAL.VehicleModel data = GetData().ElementAt(1);
+            _unitOfWork.Setup(x => x.vehicleModelRepo.GetByIdAsync(2)).ReturnsAsync(data);
+            _unitOfWork.Setup(x => x.UpdateAsync(It.IsAny<DAL.VehicleModel>())).ReturnsAsync(1);
+            _unitOfWork.Setup(x => x.CommitAsync()).ReturnsAsync(1);
+            IVehicleModelService service = new VehicleModelService(_unitOfWork.Object, _mapper);
+            var result = await service.UpdateVehicleModelAsync(2, new VehicleModelCreate { Name = "name" }).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.True(result);
+        }
+        [Fact]
+        public async Task ServiceUpdateModel_BadReq()
+        {
+            DAL.VehicleModel data = GetData().ElementAt(1);
+            _unitOfWork.Setup(x => x.vehicleModelRepo.GetByIdAsync(2)).ReturnsAsync(data);
+            _unitOfWork.Setup(x => x.UpdateAsync(It.IsAny<DAL.VehicleModel>())).ReturnsAsync(0);
+            IVehicleModelService service = new VehicleModelService(_unitOfWork.Object, _mapper);
+            var result = await service.UpdateVehicleModelAsync(2, new VehicleModelCreate { Name = "name" }).ConfigureAwait(true);
+
+            Assert.IsType<bool>(result);
+            Assert.False(result);
+        }
+
+        private static IEnumerable<DAL.VehicleModel> GetData()
+        {
+            IEnumerable<DAL.VehicleModel> data = new List<DAL.VehicleModel>
+            {
+                new DAL.VehicleModel { Id = 1, Name = "name1", Abrv = "abrv1", VehicleMakeId = 1 },
+                new DAL.VehicleModel { Id = 2, Name = "name2", Abrv = null, VehicleMakeId = 2 },
+                new DAL.VehicleModel { Id = 3, Name = "name3", Abrv = "abrv3", VehicleMakeId = 3 },
+            };
+            return data;
+        }
+    }
+}
